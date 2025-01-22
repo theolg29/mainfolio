0c657113355c7119dbf57135be577edd8b404b66
theolg <theolg36@gmail.com>
2025-01-22 20:39:40 +0100

    Fix: move sass in dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel";
-
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), tailwind(), playformCompress()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    playformCompress(),
+  ],
 
   adapter: vercel(),
 });
