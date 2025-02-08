a8aa3b746d944ce4ecf2434f59bd6e1d5e01b5e2
theolg <theolg36@gmail.com>
2025-02-08 21:57:02 +0100

    Feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,18 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import playformCompress from "@playform/compress";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://theolegourrierec.fr',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
     playformCompress(),
+    sitemap(),
   ],
   output: "static",
   adapter: vercel(),
