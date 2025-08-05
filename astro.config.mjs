189369abfc6366d49b50bfd17dbb7e5b1ec14337
theolg <theolg36@gmail.com>
2025-08-05 19:54:07 +0200

    Fix: issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,32 +33,6 @@ export default defineConfig({
   vite: {
     build: {
       cssCodeSplit: false,
-      rollupOptions: {
-        output: {
-          manualChunks: (id) => {
-            if (id.includes("node_modules")) {
-              if (id.includes("react") || id.includes("jsx-runtime")) {
-                return "react-vendor";
-              }
-              if (id.includes("motion") || id.includes("framer-motion")) {
-                return "animation-vendor";
-              }
-              if (id.includes("lottie")) {
-                return "lottie-vendor";
-              }
-              return "vendor";
-            }
-
-            if (id.includes("sections/")) {
-              return "sections";
-            }
-
-            if (id.includes("scripts/")) {
-              return "scripts";
-            }
-          },
-        },
-      },
     },
     ssr: {
       noExternal: ["motion", "framer-motion"],
